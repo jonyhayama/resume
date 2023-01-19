ce3c5646c9e9ad9f17e60335247f8cd20ece2d90
Jony Hayama <jonathan.hayama@gmail.com>
2023-01-19 16:56:37 -0300

    🎉 Extract resume into sep. repo

---
+++ nuxt.config.ts
@@ -0,0 +1,24 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  ssr: false,
+  css: [
+    '@/assets/scss/main.scss'
+  ],
+  app: {
+    head: {
+      link: [
+        {
+          rel:'icon',
+          type: 'image/png',
+          href:"/favicon.png"
+        },
+        {
+          rel:'stylesheet',
+          href:'https://use.fontawesome.com/releases/v5.7.2/css/all.css',
+          integrity:'sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr',
+          crossorigin: 'anonymous'
+        }
+      ]
+    }
+  }
+})
