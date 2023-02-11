73cf6a92a6e550118102a242cd65a581d36d0139
Jony Hayama <jonathan.hayama@gmail.com>
2023-02-11 07:17:56 -0300

    🚸 Add icon for other languages

--- components/SideButtons.vue
+++ components/SideButtons.vue
@@ -10,7 +10,17 @@ const __ = useTranslation(props.lang);
 
 <template>
   <div class="buttons">
-    <a href="#print" onclick="window.print();"><i class="fas fa-print"></i><span>{{ __('Print') }}</span></a>
-    <a href="https://jony.dev"><i class="far fa-folder-open"></i><span>{{ __('Blog') }}</span></a>
+    <div>
+      <a href="#print" onclick="window.print();"><i class="fas fa-print"></i><span>{{ __('Print') }}</span></a>
+    </div>
+    <div>
+      <a href="https://jony.dev"><i class="far fa-folder-open"></i><span>{{ __('Blog') }}</span></a>
+    </div>
+    <div v-if="props.lang === 'en-us'">
+      <NuxtLink to="/pt-br">🇧🇷<span>pt-br</span></NuxtLink>
+    </div>
+    <div v-if="props.lang === 'pt-br'">
+      <NuxtLink to="/">🇺🇸<span>en-us</span></NuxtLink>
+    </div>
   </div>
 </template>
