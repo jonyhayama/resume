6b2615336fc3b3e819e469b0db90084e0eb89090
Jony Hayama <jonathan.hayama@gmail.com>
2023-02-11 07:07:04 -0300

    🎨 Separate data from markup

--- components/WorkExperience.vue
+++ components/WorkExperience.vue
@@ -6,158 +6,185 @@ const props = defineProps({
   }
 })
 const __ = useTranslation(props.lang);
+
+const jobs = [
+  {
+    company: {
+      name: 'BairesDev',
+      segment: __('Nearshore Software Outsourcing company'),
+      url: 'https://www.bairesdev.com/'
+    },
+    position: __('Developer'),
+    dates: [{
+      from: 'Aug/2020',
+      to: 'Current',
+    }],
+    location: __('Home Office'),
+    description: __('External Client: TeraKeet, mainly workwing with Ruby on Rails and VueJS')
+  },
+  {
+    company: null,
+    position: __('Freelance PHP Developer'),
+    dates: [{
+      from: 'Jan/2016',
+      to: 'Current',
+    }],
+    location: __('Home Office'),
+    description: __('Website creation using WordPress')
+  },
+  {
+    company: {
+      name: 'Move2',
+      segment: __('Startup Accelerator'),
+      url: 'http://move2.com.br'
+    },
+    position: __('Ruby on Rails Developer'),
+    dates: [{
+      from: 'Apr/2019',
+      to: 'Aug/2020',
+    }],
+    location: __('São Paulo-SP, BRA'),
+    description: __('Engaged in: Doutor Direto, Influenciae, 99 Fórmulas, YouShow, Distrito')
+  },
+  {
+    company: {
+      name: 'EBANX',
+      segment: __('Payment Process Startup'),
+      url: 'http://ebanx.com.br'
+    },
+    position: __('PHP Developer'),
+    dates: [{
+      from: 'Dec/2018',
+      to: 'Mar/2019',
+    }],
+    location: __('Curitiba-PR, BRA'),
+    description: __('Dev Finance Team (actually worked with Elixir)')
+  },
+  {
+    company: {
+      name: 'IPM',
+      segment: __('Software House'),
+      url: 'https://www.ipm.com.br/'
+    },
+    position: __('PHP Developer'),
+    dates: [{
+      from: 'Sep/2015',
+      to: 'Dec/2015',
+    }],
+    location: __('Rio do Sul-SC, BRA'),
+    description: __('Sector: Fiscal Web')
+  },
+  {
+    company: {
+      name: 'UNIS-MG',
+      segment: __('University'),
+      url: 'https://unis.edu.br/'
+    },
+    position: __('Professor'),
+    dates: [{
+      from: 'Apr/2014',
+      to: 'Sep/2015',
+    }],
+    location: __('Varginha-MG, BRA'),
+    description: __('Subjects: Object Oriented Programming I and II, Algorithms and Programming Logic, Computing applied to logistics')
+  },
+  {
+    company: {
+      name: 'Supimpa',
+      segment: __('Digital Agency'),
+      url: 'https://www.agenciasupimpa.com.br/'
+    },
+    position: __('PHP Developer'),
+    dates: [{
+      from: 'Oct/2012',
+      to: 'Ago/2015',
+    }],
+    location: __('Varginha-MG, BRA'),
+    description: __('Engaged in: americapizza.com.br, texhyundai.com.br, aciv.com.br, lojastal.com.br, reidoseletronicos.com.br, among others.')
+  },
+  {
+    company: {
+      name: 'Tupã Comunicação',
+      segment: __('Ad Agency'),
+      url: 'https://tupacomunicacao.com.br/'
+    },
+    position: __('PHP Developer'),
+    dates: [{
+      from: 'Mar/2011',
+      to: 'Oct/2012',
+    }],
+    location: __('Varginha-MG, BRA'),
+    description: __('Engaged in: componente3.com.br, borgesefonseca.com.br, among others.')
+  },
+  {
+    company: {
+      name: 'Q1',
+      segment: __('Digital Agency'),
+      url: 'http://q1agenciadigital.com.br'
+    },
+    position: __('PHP Developer'),
+    dates: [{
+      from: 'Mar/2011',
+      to: 'Ago/2011',
+    }],
+    location: __('Varginha-MG, BRA'),
+    description: __('Engaged in: paroquiadodivino.com.br, topcarlavras.com.br, among others.')
+  },
+  {
+    company: {
+      name: 'One Up',
+      segment: __('Travel Agency'),
+      url: 'http://workandtravel.com.br/'
+    },
+    position: __('Program Assistant'),
+    dates: [{
+      from: 'Apr/2007',
+      to: 'Mar/2012',
+    }],
+    location: __('Varginha-MG, BRA'),
+    description: __('Duties: Gather and revise documents, send documents to Visa Sponsors, help participants to schedule their visa appointments and fill out the required documents.')
+  },
+  {
+    hidden: true,
+    company: {
+      name: 'ICC Informática',
+      segment: __('Computing School'),
+      url: 'http://workandtravel.com.br/'
+    },
+    position: __('Teacher'),
+    dates: [{
+      from: 'Mar/2004',
+      to: 'Dez/2006',
+    }, {
+      from: 'Abr/2007',
+      to: 'Set/2007'
+    }],
+    location: __('Varginha-MG, BRA'),
+    description: __('Subjects: Windows, MS Office, CorelDraw, Photoshop, Webdesign')
+  },
+]
 </script>
 
 <template>
   <h2><i class="fas fa-briefcase"></i>{{ __('Work Experience') }}</h2>
   <table>
-    <tr>
+    <tr v-for="job in jobs" :class="{ hidden: job.hidden }">
       <td>
-        <i class="far fa-calendar-alt"></i>{{ __('Aug/2020 ~ Current') }}
-        <br /><i class="fa fa-map-marker-alt"></i>{{ __('Home Office') }}
+        <template v-for="date in job.dates">
+          <i class="far fa-calendar-alt"></i>{{ __(`${date.from} ~ ${date.to}`) }}<br />
+        </template>
+        <i class="fa fa-map-marker-alt"></i>{{ job.location }}
       </td>
       <td>
-        <strong>{{ __('Developer at') }} <a href="https://www.bairesdev.com/" target="_blank"
-            rel="nofollow">BairesDev</a></strong>
-        <br />{{ __('Nearshore Software Outsourcing company') }}
+        <strong>{{ job.position }}
+          <template v-if="job.company">
+            | <a :href="job.company.url" target="_blank" rel="nofollow">{{ job.company.name }}</a>
+          </template>
+        </strong>
+        <br />
+        {{ job.company && job.company.segment }}
         <div>
-          {{ __('External Client: TeraKeet, mainly workwing with') }} <a href="https://rubyonrails.org/" target="_blank"
-            rel="nofollow">Ruby on Rails</a> and <a href="https://vuejs.org/" target="_blank"
-            rel="nofollow">VueJS</a>
-        </div>
-      </td>
-    </tr>
-    <tr>
-      <td>
-        <i class="far fa-calendar-alt"></i>{{ __('Jan/2016 ~ Current') }}
-        <br /><i class="fa fa-map-marker-alt"></i>{{ __('Home Office') }}
-      </td>
-      <td>
-        <strong>{{ __('Freelance PHP Developer') }}</strong>
-        <br />{{ __('Website creation using') }} <a href="https://wordpress.org/" target="_blank" rel="nofollow">WordPress</a>
-      </td>
-    </tr>
-    <tr>
-      <td>
-        <i class="far fa-calendar-alt"></i>{{ __('Apr/2019 ~ Aug/2020') }}
-        <br /><i class="fa fa-map-marker-alt"></i>{{ __('São Paulo-SP, BRA') }}
-      </td>
-      <td>
-        <strong>{{ __('Ruby on Rails Developer at') }} <a href="http://move2.com.br" target="_blank"
-            rel="nofollow">Move2</a></strong>
-        <br />{{ __('Startup Accelerator') }}
-        <div>
-          {{ __('Engaged in: Doutor Direto, Influenciae, 99 Fórmulas, YouShow, Distrito') }}
-        </div>
-      </td>
-    </tr>
-    <tr>
-      <td>
-        <i class="far fa-calendar-alt"></i>{{ __('Dec/2018 ~ Mar/2019') }}
-        <br /><i class="fa fa-map-marker-alt"></i>{{ __('Curitiba-PR, BRA') }}
-      </td>
-      <td>
-        <strong>{{ __('PHP Developer at') }} <a href="http://ebanx.com.br" target="_blank" rel="nofollow">EBANX</a> {{ __('(actually worked with Elixir)') }}</strong>
-        <br />{{ __('Payment Process Startup') }}
-        <div>
-          {{ __('Dev Finance Team') }}
-        </div>
-      </td>
-    </tr>
-    <tr>
-      <td>
-        <i class="far fa-calendar-alt"></i>{{ __('Sep/2015 ~ Dec/2015') }}
-        <br /><i class="fa fa-map-marker-alt"></i>{{ __('Rio do Sul-SC, BRA') }}
-      </td>
-      <td>
-        <strong>{{ __('PHP Developer at') }} <a href="https://www.ipm.com.br/" target="_blank" rel="nofollow">IPM</a></strong>
-        <br />{{ __('Software House') }}
-        <div>
-          {{ __('Sector: Fiscal Web') }}
-        </div>
-      </td>
-    </tr>
-    <tr>
-      <td>
-        <i class="far fa-calendar-alt"></i>{{ __('Apr/2014 ~ Sep/2015') }}
-        <br /><i class="fa fa-map-marker-alt"></i>{{ __('Varginha-MG, BRA') }}
-      </td>
-      <td>
-        <strong>{{ __('Professor at') }} <a href="https://unis.edu.br/" target="_blank" rel="nofollow">UNIS-MG</a></strong>
-        <br />{{ __('University') }}
-        <div>
-          {{ __('Subjects: Object Oriented Programming I and II, Algorithms and Programming Logic, Computing applied to logistics') }}
-        </div>
-      </td>
-    </tr>
-    <tr>
-      <td>
-        <i class="far fa-calendar-alt"></i>{{ __('Oct/2012 ~ Ago/2015') }}
-        <br /><i class="fa fa-map-marker-alt"></i>{{ __('Varginha-MG, BRA') }}
-      </td>
-      <td>
-        <strong>{{ __('PHP Developer at') }} <a href="https://www.agenciasupimpa.com.br/" target="_blank"
-            rel="nofollow">Supimpa</a></strong>
-        <br />{{ __('Digital Agency') }}
-        <div>
-          {{ __('Engaged in: americapizza.com.br, texhyundai.com.br, aciv.com.br, lojastal.com.br, reidoseletronicos.com.br, among others.') }}
-        </div>
-      </td>
-    </tr>
-    <tr>
-      <td>
-        <i class="far fa-calendar-alt"></i>{{ __('Mar/2011 ~ Oct/2012') }}
-        <br /><i class="fa fa-map-marker-alt"></i>{{ __('Varginha-MG, BRA') }}
-      </td>
-      <td>
-        <strong>{{ __('PHP Developer at') }} <a href="https://tupacomunicacao.com.br/pt/home/" target="_blank"
-            rel="nofollow">Tupã Comunicação</a></strong>
-        <br />{{ __('Ad Agency') }}
-        <div>
-          {{ __('Engaged in: componente3.com.br, borgesefonseca.com.br, among others.') }}
-        </div>
-      </td>
-    </tr>
-    <tr>
-      <td>
-        <i class="far fa-calendar-alt"></i>{{ __('Mar/2011 ~ Ago/2011') }}
-        <br /><i class="fa fa-map-marker-alt"></i>{{ __('Varginha-MG, BRA') }}
-      </td>
-      <td>
-        <strong>{{ __('PHP Developer at') }} <a href="http://q1agenciadigital.com.br" target="_blank"
-            rel="nofollow">Q1</a></strong>
-        <br />{{ __('Digital Agency') }}
-        <div>
-          {{ __('Engaged in: paroquiadodivino.com.br, topcarlavras.com.br, among others.') }}
-        </div>
-      </td>
-    </tr>
-    <tr>
-      <td>
-        <i class="far fa-calendar-alt"></i>{{ __('Apr/2007 ~ Mar/2012') }}
-        <br /><i class="fa fa-map-marker-alt"></i>{{ __('Varginha-MG, BRA') }}
-      </td>
-      <td>
-        <strong>{{ __('Program Assistant at') }} <a href="http://workandtravel.com.br/" target="_blank" rel="nofollow">One
-            Up</a></strong>
-        <br />{{ __('Travel Agency') }}
-        <div>
-          {{ __('Duties: Gather and revise documents, send documents to Visa Sponsors, help participants to schedule their visa appointments and fill out the required documents.') }}
-        </div>
-      </td>
-    </tr>
-    <tr class="hidden">
-      <td>
-        <i class="far fa-calendar-alt"></i>{{ __('Mar/2004 ~ Dez/2006') }}
-        <br /><i class="far fa-calendar-alt"></i>{{ __('Abr/2007 ~ Set/2007') }}
-        <br /><i class="fa fa-map-marker-alt"></i>{{ __('Varginha-MG, BRA') }}
-      </td>
-      <td>
-        <strong>{{ __('Teacher at') }} <a href="http://grupoicc.com.br/" target="_blank" rel="nofollow">ICC
-            Informática</a></strong>
-        <br />{{ __('Computing School') }}
-        <div>
-          {{ __('Subjects: Windows, MS Office, CorelDraw, Photoshop, Webdesign') }}
+          {{ job.description }}
         </div>
       </td>
     </tr>
